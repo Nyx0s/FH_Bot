e734d21e94af79f2505aae93b2c7d91d8ca48fa1
Nyx0s <maximilian@walfisch.eu>
2023-10-18 01:14:14 +0200

    Main Branch STart

--- Modules/ical_checker.py
+++ Modules/ical_checker.py
@@ -7,29 +7,32 @@ import os
 load_dotenv()  # Load .env file
 
 def check_calendar_for_date(date_to_check):
+    # Retrieve the iCal file from the URL specified in the .env file
     url = os.getenv('ICAL_URL')
-    # iCal-Datei von der URL abrufen
     response = requests.get(url)
 
     if response.status_code == 200:
         ical_text = response.text
 
-        # Kalenderobjekt erstellen
+        # Create a calendar object from the iCal text
         cal = Calendar.from_ical(ical_text)
 
-        # Datum, das Sie überprüfen möchten
+        # Parse the date you want to check
         date_to_check = datetime.strptime(date_to_check, '%Y-%m-%d')
 
-        # Überprüfen, ob es Einträge an diesem Datum gibt
+        # Check for entries on the specified date
         for event in cal.walk('VEVENT'):
             event_start = event.get('DTSTART').dt
             if event_start.date() == date_to_check.date():
+                # If an entry is found on the specified date, return its details
                 return event.get('summary'), event.get('description'), event.get('dtstart').dt, event.get('dtend').dt
-    return None  # Kein Eintrag an diesem Datum gefunden
+    return None  # No entry found on this date
 
-if __name__ == "__main":
-    termine = check_calendar_for_date("2023-10-17")
+if __name__ == "__main__":
+    # Check the calendar for entries on the specified date
+    termine = check_calendar_for_date("2023-10-19")
     if termine:
-        print(f"Termin: {termine[0]}\nBeschreibung: {termine[1]}\nStartzeit: {termine[2]}\nEndzeit: {termine[3]}\n")
+        # If entries are found, print their details in English
+        print(f"Event: {termine[0]}\nDescription: {termine[1]}\nStart Time: {termine[2]}\nEnd Time: {termine[3]}\n")
     else:
-        print("Kein Termin gefunden.")
+        print("No event found.")  # No entry found on the specified date
--- fh_bot.py
+++ fh_bot.py
@@ -7,26 +7,28 @@ import Modules.ical_checker as ical_checker
 
 class MyClient(discord.Client):
     async def on_ready(self):
+        # This function is called when the bot is ready and logged in.
         print(f'Logged on as {self.user}!')
 
     async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
+        # This function is called when the bot receives a message.
+        # It logs information about the received message.
+        logging.info(f'Message from {message.author}: {message}')
 
     def get_cuurent_date(self):
         while True:
             current_time = time.strftime('%Y-%m-%d %H:%M:%S')
 
-
             if current_time.split(" ")[1] == "20:32:00":
+                # If the current time is 20:32:00, check the calendar for events.
                 termine = ical_checker.check_calendar_for_date(current_time.split(" ")[0])
                 if termine:
-                    print(f"Termin: {termine[0]}\nBeschreibung: {termine[1]}Startzeit: {termine[2]}\nEndzeit: {termine[3]}\n")
+                    # If events are found, print event details.
+                    print(f"Termin: {termine[0]}\nBeschreibung: {termine[1]}\nStartzeit: {termine[2]}\nEndzeit: {termine[3]}\n")
                     continue
 
-
-
-
-            #time.sleep(60)  # Sleep for 60 seconds (1 minute)
+            # Uncomment the next line if you want to add a 60-second delay between checks.
+            # time.sleep(60)
 
 try:
     load_dotenv()  # Load .env file
@@ -38,10 +40,10 @@ try:
 
         intents = discord.Intents.default()
         intents.message_content = True
-        discord.utils.setup_logging(level=logging.INFO, root=True)
+        discord.utils.setup_logging(level=logging.INFO, root=False)
         client = MyClient(intents=intents)
 
-
+        # Start the bot and the get_cuurent_date function.
         client.run(token)
         client.get_cuurent_date()
 
